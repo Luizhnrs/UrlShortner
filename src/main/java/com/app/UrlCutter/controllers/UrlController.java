7f03210f1ebcc551db465ac8fb842796f481a66c
Luizhnrs <Luizhnrsg@gmail.com>
2024-10-29 21:47:18 -0300

    Feat: Redirect Url implemented

--- src/main/java/com/app/UrlCutter/controllers/UrlController.java
+++ src/main/java/com/app/UrlCutter/controllers/UrlController.java
@@ -3,8 +3,10 @@ package com.app.UrlCutter.controllers;
 import com.app.UrlCutter.entities.Url;
 import com.app.UrlCutter.repositories.UrlRepository;
 import com.app.UrlCutter.requests.UrlRequest;
+import jakarta.servlet.http.HttpServletRequest;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,13 +23,16 @@ public class UrlController {
 
 
     @PostMapping(value = "/urlcutter")
-    public ResponseEntity<Void> urlCutter(@RequestBody UrlRequest request) {
+    public ResponseEntity<Void> urlCutter(@RequestBody UrlRequest request,
+                                          HttpServletRequest servletRequest) {
         String id;
         do {
            id = RandomStringUtils.randomAlphanumeric(5, 10);
         } while (urlRepository.existsById(id));
 
         urlRepository.save(new Url(id, request.url(), LocalDateTime.now().plusMinutes(1)));
+        var redirectUrl = servletRequest.getRequestURL().toString().replace("urlcutter/", id);
+
         return ResponseEntity.ok().build();
     }
 }
