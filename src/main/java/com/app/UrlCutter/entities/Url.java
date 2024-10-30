558cc2acc99dd52192ed4311d57e16f988655466
Luizhnrs <Luizhnrsg@gmail.com>
2024-10-30 13:36:19 -0300

    FIX: Getter and Setter is created

--- src/main/java/com/app/UrlCutter/entities/Url.java
+++ src/main/java/com/app/UrlCutter/entities/Url.java
@@ -24,4 +24,28 @@ public class Url {
         this.fullUrl = fullUrl;
         this.expiresAt = expiresAt;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFullUrl() {
+        return fullUrl;
+    }
+
+    public void setFullUrl(String fullUrl) {
+        this.fullUrl = fullUrl;
+    }
+
+    public LocalDateTime getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(LocalDateTime expiresAt) {
+        this.expiresAt = expiresAt;
+    }
 }
